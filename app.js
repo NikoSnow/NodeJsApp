dbad0adf9380fe9f5ff5ad15536a9b7cb47f9e98
Nicolas <nicolas.gomezo@correo.usa.edu.co>
2018-03-10 15:49:37 -0500

    Conexion a la base de datos

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//Modelo de las Tablas
+var models = require('./models/index');
+
 var index = require('./routes/index');
 
 var app = express();
@@ -22,6 +26,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+models.sequelize.sync().then(
+  function(){
+    console.log("Se conecto a la DB")
+  }
+)
+.catch(
+  function(error){
+    console.log(err, "Error en la conexion");
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
